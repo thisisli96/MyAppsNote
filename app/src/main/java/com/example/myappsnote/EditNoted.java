e03c3c87356c0424ab7398ea158bb2a199ec8a6c
muh ikhsan <thisisli96@yahoo.com>
2020-01-11 14:17:27 +0700

    step2 pembuatan activity untuk edit text yang di klik

---
+++ app/src/main/java/com/example/myappsnote/EditNoted.java
@@ -0,0 +1,29 @@
+package com.example.myappsnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditNoted extends AppCompatActivity {
+
+    TextView textchange;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_noted);
+
+       // textchange = findViewById(R.id.textchange);
+        editText = findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        final String positionlist = bundle.getString("position");
+       // textchange.setText(positionlist);
+        editText.setText(positionlist);
+
+    }
+}
--- app/src/main/java/com/example/myappsnote/MainActivity.java
+++ app/src/main/java/com/example/myappsnote/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.myappsnote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -23,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, noted);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, noted.toString(), Toast.LENGTH_SHORT).show(); // to see list array yang di klik
+                Intent intent = new Intent (getApplicationContext(), EditNoted.class);
+                intent.putExtra("position",noted.get(position));
+                startActivity(intent);
+            }
+        });
 
     } // on create
 
